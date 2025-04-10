7a62071c4de35099e2fe43869f0c0bcf865c74ba
sujanr <sujanrumakantha@gmail.com>
2025-04-10 10:37:03 +0530

    added 13

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(cors({
 }))
 
 app.get("/",(req,res)=>{
-    res.status(200).json({success:true,message:"Hello World 12"})
+    res.status(200).json({success:true,message:"Hello World 13"})
 })
 
 app.listen(5000,()=>{
